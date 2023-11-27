28202cdb3a73bde6ccbbf1ad9aa83b6370e6783d
VannWasHere <ateen1203@gmail.com>
2023-11-27 18:13:23 +0700

    Adding Counter Length

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,21 +1,35 @@
 import Button from "./Button";
+import { useState } from "react";
 
 const FormInput = () => {
+    
+    const [getCounter, setCounter] = useState(50);
+
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        const maxValue = event.target.maxLength;
+        const remainingChar = maxValue-value.length;
+        setCounter(remainingChar)
+    }
+
     return (
         <div className="w-full p-5 border rounded-lg">
             <h1 className="text-3xl font-bold">Input Notes</h1>
             <form method="post" required className="mt-4 flex flex-col justify-center">
-                <p maxLength="50" id="title-length" className="flex justify-end mb-2 text-gray-400">50 Chars Left</p>
+                <p id="title-length" className="flex justify-end mb-2 text-gray-400">{getCounter} Chars Left</p>
                 <input 
-                type="text" 
+                type="text"
                 className="bg-yellow-200 p-2 rounded-xl"
-                placeholder="Note Title"/> <br />
+                maxLength={50}
+                onChange={handleInputChange}
+                placeholder="Note Title"/> 
+                <br />
                 
                 <textarea 
                 type="text"
                 draggable="false" 
                 rows={8}
-                className="bg-yellow-200 p-2 rounded-xl resize-none"
+                className="bg-yellow-200 p-2 rounded-xl mb-3 resize-none"
                 placeholder="Note Title"/>  
 
                 <Button text={"Submit Form"}/>
