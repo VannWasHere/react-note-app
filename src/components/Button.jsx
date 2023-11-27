64f62500b6f08305715857ba52f106ff12d96fae
VannWasHere <ateen1203@gmail.com>
2023-11-27 23:57:17 +0700

    Adjusting CSS on Form and Button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const Button = (props) => {
-    return <button type={props.type} className='mt-2 mb-2 bg-yellow-500 p-2 rounded-2xl text-white' >{props.text}</button>
+    return <button type={props.type} className='mt-2 mb-2 bg-yellow-500 p-3 rounded-2xl text-white transition-all hover:bg-yellow-700' >{props.text}</button>
 }
 
 Button.propTypes = {
