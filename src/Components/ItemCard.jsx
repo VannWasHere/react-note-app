dff337fb7f446a804cb8962685b82550dd93b188
VannWasHere <ateen1203@gmail.com>
2023-12-29 18:43:05 +0700

    Add little things

--- src/Components/ItemCard.jsx
+++ src/Components/ItemCard.jsx
@@ -30,12 +30,14 @@ const ItemCard = ({ notes, onNotesUpdate }) => {
         });
         onNotesUpdate(editedData);
     }
+
     return (
         <>
             <h1 className="mt-2 text-2xl font-bold uppercase">OnGoing Notes</h1>
             <div className="mt-2 grid grid-cols-3 gap-8">
-                {
-                    ongoingNotes.map((note, key) =>(
+                {ongoingNotes.length === 0 ? (
+                    <span className="p-5 bg-yellow-600 rounded-2xl text-white font-bold w-full">No ongoing notes available.</span>
+                ) : ongoingNotes.map((note, key) => (
                         <section key={key} className="bg-slate-800 text-white p-4 rounded-lg">
                             <div>
                                 <h1 className="text-xl">{note.title}</h1>
@@ -52,9 +54,10 @@ const ItemCard = ({ notes, onNotesUpdate }) => {
             </div>
             <h1 className="mt-8 text-2xl text-indigo-700 font-bold uppercase">Archived Notes</h1>
             <div className="mt-2 grid grid-cols-3 gap-8">
-                {
-                    archivedNotes.map((note, key) =>(
-                        <section key={key} className="bg-indigo-900 text-white p-4 rounded-lg">
+                {archivedNotes.length === 0 ? (
+                    <span className="p-5 bg-yellow-600 rounded-2xl text-white font-bold w-full">No archived notes right now.</span>
+                ) : archivedNotes.map((note, key) => (
+                        <section key={key} className="bg-slate-800 text-white p-4 rounded-lg">
                             <div>
                                 <h1 className="text-xl">{note.title}</h1>
                                 <h3 className="text-slate-400 mb-2">{note.createdAt}</h3>
@@ -62,7 +65,7 @@ const ItemCard = ({ notes, onNotesUpdate }) => {
                             </div>
                             <div className="flex gap-5 mt-4">
                                 <Button clickStatement={() => {deleteNotes(note.id)}} Text={"Delete"}/>
-                                <Button clickStatement={() => {moveNotes(note.id)}} Text={"Open Notes"}/>
+                                <Button clickStatement={() => {moveNotes(note.id)}} Text={"Archive"}/>
                             </div>
                         </section>
                     ))
